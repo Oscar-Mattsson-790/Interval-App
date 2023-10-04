d93b1f4d2ec5cf58774b4fa1e21fc36120bb1f07
Oscar Mattsson <oscar.mattsson.790@gmail.com>
2023-10-04 09:03:37 +0200

    fix: styles digital-timer

--- interval-app/src/componets/DigitalTimerView/DigitalTimerView.jsx
+++ interval-app/src/componets/DigitalTimerView/DigitalTimerView.jsx
@@ -3,30 +3,42 @@ import "./DigitalTimerView.scss";
 import { useNavigate } from "react-router-dom";
 
 const DigitalTimerView = () => {
-  const [secondsLeft, setSecondsLeft] = useState(60);
+  const [minutesLeft, setMinutesLeft] = useState(1); // set initial minute
+  const [secondsLeft, setSecondsLeft] = useState(0); // set initial second
   const navigate = useNavigate();
 
   useEffect(() => {
     const timerID = setInterval(() => {
-      setSecondsLeft((prev) => prev - 1);
+      setSecondsLeft((prevSeconds) => {
+        if (prevSeconds === 0) {
+          setMinutesLeft((prevMinutes) => prevMinutes - 1);
+          return 59;
+        } else {
+          return prevSeconds - 1;
+        }
+      });
     }, 1000);
 
-    if (secondsLeft === 0) {
+    if (minutesLeft === 0 && secondsLeft === 0) {
       navigate("/alarm");
       clearInterval(timerID);
     }
 
     return () => clearInterval(timerID);
-  }, [secondsLeft, navigate]);
+  }, [minutesLeft, secondsLeft, navigate]);
 
   const cancelTimer = () => {
     navigate("/set-timer");
   };
 
+  const displaySeconds = secondsLeft < 10 ? `0${secondsLeft}` : secondsLeft;
+
   return (
     <div className="digital-timer-view">
-      <h1>interval</h1>
-      <p>{secondsLeft}</p>
+      <h1>Interval</h1>
+      <p className="time">
+        {minutesLeft}:{displaySeconds}
+      </p>
       <button onClick={cancelTimer}>ABORT TIMER</button>
     </div>
   );
