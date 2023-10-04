19ba299bb1cf8134e5491037f7b3a1bb368d59e1
Oscar Mattsson <oscar.mattsson.790@gmail.com>
2023-10-04 10:37:11 +0200

    feat: added alarmView

--- interval-app/src/componets/AlarmView/AlarmView.jsx
+++ interval-app/src/componets/AlarmView/AlarmView.jsx
@@ -1,9 +1,14 @@
 import "./Alarmview.scss";
+import bellLogo from "../../assets/notification.png";
 
 const AlarmView = () => {
   return (
     <div className="alarm-view">
-      <h1>Alarm View</h1>
+      <div className="content-container">
+        <img className="bellLogo" src={bellLogo} alt="bellLogo" />
+        <h1>Times up!</h1>
+      </div>
+      <button>SET NEW TIMER</button>
     </div>
   );
 };
