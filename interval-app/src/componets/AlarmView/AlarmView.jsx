7ed3c4286daedcfa99cf280df202a20faa4f188b
Oscar Mattsson <oscar.mattsson.790@gmail.com>
2023-10-04 17:04:30 +0200

    feat: add nav from alarm to setTimer

--- interval-app/src/componets/AlarmView/AlarmView.jsx
+++ interval-app/src/componets/AlarmView/AlarmView.jsx
@@ -1,14 +1,21 @@
 import "./Alarmview.scss";
 import bellLogo from "../../assets/notification.png";
+import { useNavigate } from "react-router-dom";
 
 const AlarmView = () => {
+  const navigate = useNavigate();
+
+  const goToSetTimer = () => {
+    navigate("/set-timer");
+  };
+
   return (
     <div className="alarm-view">
       <div className="content-container">
         <img className="bellLogo" src={bellLogo} alt="bellLogo" />
         <h1>Times up!</h1>
       </div>
-      <button>SET NEW TIMER</button>
+      <button onClick={goToSetTimer}>SET NEW TIMER</button>
     </div>
   );
 };
